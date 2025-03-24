f621c326d899dc09218531b57c06249d152740a8
Zhexun <ZLIM116@e.ntu.edu.sg>
2025-03-24 18:00:40 +0800

    update app

--- assignment_2/app.py
+++ assignment_2/app.py
@@ -18,28 +18,25 @@ results_container = st.container()
 with search_container:
     if query := st.chat_input("Type in a search query."):
         st.session_state["search_results"] = []
-        results = github_api.search_github(query, num_requests=20)
+        results = github_api.search_github(query, num_requests=30)
 
         for r in results:
             summary = github_api.get_repo_summary(r["full_name"])
             if not summary: continue
             created_date = utils.get_month_year(r["created_at"])
-            url = "https://github.com/" + r["full_name"]
+            url = r["url"]
             owner = r["owner"]["login"]
             repo_name = r["name"]
+            
 
-            # Append the result to session state
             st.session_state["search_results"].append({"created_date": created_date, "url":url, "repo_name":repo_name,"owner":owner,"summary":summary})
 
-            # Display the result immediately
             with results_container:
                 with st.container(border=True):
                     st.subheader(repo_name)
                     st.write(f"Owner: {owner} | Created: {created_date} | url: {url}")
                     st.write("Summary:")
                     st.write(summary)
-            # st.experimental_rerun()
-            
 
 
 with results_container:
